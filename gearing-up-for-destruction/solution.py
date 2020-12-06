95831f213ea78db445ba8cac46eb38c7f42a65cd
not-that-python <67548289+not-that-python@users.noreply.github.com>
2020-12-06 13:39:44 +0000

    Update solution.py

--- gearing-up-for-destruction/solution.py
+++ gearing-up-for-destruction/solution.py
@@ -1,44 +1,32 @@
 def solution(pegs):
-    len_pegs = len(pegs)
-    dist=[pegs[i+1]-pegs[i] for i in range(len_pegs-1)]
-    
-    rn=0
-    for i in range(len_pegs-1):
-        if i%2==0:
-            rn+=dist[i]
+    difs = [pegs[i+1] - pegs[i] for i in range(len(pegs) - 1)]
+    a = 0
+    for i in range(len(difs)):
+        if i%2 != 0:
+            a -= difs[i]
         else:
-            rn-=dist[i]
-    r0=2*rn
-       
-    if rn <= 1:
-        return [-1,-1]
-    
-    if len_pegs%2==0:
-        if r0/3>=dist[0]-1:
-            return[-1,-1]
-        if rn/3>=dist[-1]-1:
-            return[-1,-1]
-        
-        a=int(r0/3)   
-        rs=[a]*len_pegs
-        for i in range(len_pegs-1):
-            rs[i+1]=dist[i]-rs[i]
-            
-        for q in rs:
-            if q<=0:
-                return[-1,-1]
-                
-        if r0%3==0:
-            r0=int(r0/3)
-            dr=1
-        else:
-            dr=3
-        
+            a += difs[i]
+    A = 2*a
+    if a<1:
+        return[-1, -1]
+    if len(pegs) % 2!= 0:
+        b = 1
     else:
-        if r0>=dist[0]-1:
-            return[-1,-1]
-        if rn>=dist[-1]-1:
-            return[-1,-1]  
-        dr=1
-    
-    return[r0,dr]
+        
+        r0=A/3
+        
+        rs=[r0]*len(pegs)
+        
+        for i in range(len(difs)):
+            rs[i+1] = difs[i] - rs[i]
+        for i in rs:
+            if i <= 0:
+                return[-1, -1]
+        
+        if A%3 == 0:
+            A /= 3
+            b = 1
+        else:
+            b = 3
+        
+    return [A, b]
