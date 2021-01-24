b8ce92f9f56b63703973dc24c290b91f8169c1c3
not-that-python <67548289+not-that-python@users.noreply.github.com>
2021-01-24 16:52:46 +0000

    Update solution.py

--- minion-labour-shifts/solution.py
+++ minion-labour-shifts/solution.py
@@ -1,3 +1,4 @@
 def solution(data, n):
     new_data = [i for i in data if data.count(i) <= n]
+    # This makes a new list out of data called 'new_data', which checks all the elements in data if the number of the same element within is less than n.
     return new_data
